d7e9aac3a7eb1431f7b9213d73b359edf22741d4
Michael Cullen <mjcullen2015@gmail.com>
2025-01-12 18:50:48 +0000

    removing rasa

--- backend/chat/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import chat_view, chat_api
-
-
-urlpatterns = [
-    path('', chat_view, name='chat'),
-    path('api/chat/', chat_api, name='chat_api'),
-]
