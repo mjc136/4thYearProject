c50f268b7945cfbd9f535f08dd071d0191fd470d
Michael Cullen <mjcullen2015@gmail.com>
2025-01-16 22:48:22 +0000

    add methods to user states

--- bot/state/user_state.py
+++ bot/state/user_state.py
@@ -1,5 +1,16 @@
-from botbuilder.core import UserState
+class UserState:
+    def __init__(self):
+        # Language proficiency attributes
+        self.language = None  # Current language the user is learning
+        self.proficiency_level = None  # Beginner, Intermediate, Advanced
 
-class UserProfile:
-    def __init__(self, language="english"):
+    def set_language(self, language):
+        """Set the current language the user is learning."""
         self.language = language
+
+    def set_proficiency_level(self, level):
+        """Set the user's proficiency level."""
+        if level in ["Beginner", "Intermediate", "Advanced"]:
+            self.proficiency_level = level
+        else:
+            raise ValueError("Invalid proficiency level. Choose Beginner, Intermediate, or Advanced.")
